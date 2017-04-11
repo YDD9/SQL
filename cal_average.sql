7642f2b1c926a9fa0ca7da8f0ca5ae319b682c55
YDD9 <hushuai2003cn@hotmail.com>
2017-04-12 00:04:35 +0200

    Create cal_average.sql

---
+++ cal_average.sql
@@ -0,0 +1,23 @@
+# http://www.w3resource.com/sql-exercises/sql-retrieve-exercise-26.php
+
+/*
+Write a SQL query to calculate the average price of all products of the manufacturer which code is l6.
+
+Sample table : item_mast
+
+ PRO_ID PRO_NAME                   PRO_PRICE    PRO_COM
+------- ------------------------- ---------- ----------
+    101 Mother Board                    3200         15
+    102 Key Board                        450         16
+    103 ZIP drive                        250         14
+    104 Speaker                          550         16
+    105 Monitor                         5000         11
+    106 DVD drive                        900         12
+    107 CD drive                         800         12
+    108 Printer                         2600         13
+    109 Refill cartridge                 350         13
+    110 Mouse                            250         12
+*/
+
+SELECT AVG(pro_price) FROM item_mast   
+  WHERE pro_com=16;  
