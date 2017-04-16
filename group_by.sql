e83ca46f65f423a988fc295737acdb712d75e86c
YDD9 <hushuai2003cn@hotmail.com>
2017-04-16 12:25:54 +0200

    Create group_by.sql

---
+++ group_by.sql
@@ -0,0 +1,25 @@
+# http://www.w3resource.com/sql-exercises/sql-aggregate-function-exercise-10.php
+
+/*
+Write a SQL statement to find the highest purchase amount ordered by the each customer on a particular date with their ID, order date and highest purchase amount.
+
+ord_no      purch_amt   ord_date    customer_id  salesman_id
+----------  ----------  ----------  -----------  -----------
+70001       150.5       2012-10-05  3005         5002
+70009       270.65      2012-09-10  3001         5005
+70002       65.26       2012-10-05  3002         5001
+70004       110.5       2012-08-17  3009         5003
+70007       948.5       2012-09-10  3005         5002
+70005       2400.6      2012-07-27  3007         5001
+70008       5760        2012-09-10  3002         5001
+70010       1983.43     2012-10-10  3004         5006
+70003       2480.4      2012-10-10  3009         5003
+70012       250.45      2012-06-27  3008         5002
+70011       75.29       2012-08-17  3003         5007
+70013       3045.6      2012-04-25  3002         5001
+
+*/
+
+SELECT customer_id,ord_date,MAX(purch_amt)   
+FROM orders   
+GROUP BY customer_id,ord_date;  
